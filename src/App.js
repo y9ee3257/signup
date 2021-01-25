816c4158145aa11ca91e0cd88be4c10cdecb8af7
ravi teja thummala <rt.usapp@gmail.com>
2021-01-24 21:40:52 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home/Home';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Home></Home>
-      </div >
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     )
   }
 }
